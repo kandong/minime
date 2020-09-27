e950bfaa7ff9ba5999129e08af5b32b101d8a81c
Kan Dong <kannier.dong@gmail.com>
2020-09-27 14:47:02 -0700

    Updates

---
+++ precache-manifest.ce0d33c1d02833ce6a9653cd6bec14e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd72fb49263fcadf3ea1a2a365daec89",
+    "url": "//minime/index.html"
+  },
+  {
+    "revision": "2aaeeda7fb993c34120d",
+    "url": "//minime/static/css/main.5bbd338c.chunk.css"
+  },
+  {
+    "revision": "d633a8a27c9e79a999fe",
+    "url": "//minime/static/js/2.262be79a.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "//minime/static/js/2.262be79a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aaeeda7fb993c34120d",
+    "url": "//minime/static/js/main.2ba9f0b7.chunk.js"
+  },
+  {
+    "revision": "7b3f531e4f3219ee818e",
+    "url": "//minime/static/js/runtime-main.9d3785ec.js"
+  }
+]);
